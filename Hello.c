ba03f45ef53abeb0845ce2f92a64a89e060280db
Supanya-Fukloy <69011176@kmitl.ac.th>
2026-09-23 13:52:08 +0700

    Add farewell2

--- Hello.c
+++ Hello.c
@@ -2,12 +2,18 @@
 
 int main(void)
 {
-    greeting("“Developer A,B,C !!”");
+    greeting("Developer A,B,C !!");
+    farewell("Developer A,B,C");
     return 0;
 }
 // Print a greeting message for the specified user
 int greeting(const char *name)
 {
 printf("Hello, %s!\n", name);
+return 0;
+}
+int farewell(const char *name)
+{
+printf("Goodbye, %s!\n", name);
 return 0;
 }
