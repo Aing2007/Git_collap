47ee7c68b457441c2827ed2f1828943a4747f131
69011185 <69011185@kmitl.ac.th>
2026-09-23 13:38:53 +0700

    add funtion

--- Hello.c
+++ Hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
 int main(void)
 {
-printf("Hello, Git Collaboration!\n");
+    greeting("“Developer A,B,C !!”");
+    return 0;
+}
+int greeting(const char *name)
+{
+printf("Hello, %s!\n", name);
 return 0;
 }
